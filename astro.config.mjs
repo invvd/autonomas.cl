69e65be79b68618792f1748f2a316b001b995559
Danny <danymassmcf@gmail.com>
2026-07-09 03:04:31 -0400

    chore: update dependencies and remove blog collection
    - downgraded @astrojs/vercel from ^11.0.2 to ^8.0.4
    - updated react-dom from ^19.0.0 to ^19.2.7
    - removed blog collection configuration from content.config.ts
    - added .npmrc file to set legacy-peer-deps to true

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://autonomas.cl/",
-  adapter: vercel(),
   integrations: [tailwind(), icon(), sitemap(), partytown({
     config: {
       forward: ["dataLayer.push"],
